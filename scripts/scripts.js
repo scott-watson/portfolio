97545781cdd37dba91d5502f0ba97769772c233a
S Watson <scottwatson1985@live.com>
2023-02-13 23:07:59 +0000

    getting closer, need to get the menu icon to now drop down the nav items

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,19 @@
-const menuIcon = document.getElementById("navOne");
-const menu = document.getElementById("navThree");
-
-menuIcon.addEventListener("click", () => {
-  menu.classList.toggle("active");
-});
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('navTwo')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
