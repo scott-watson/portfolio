0ad80151c7fb45b56689625a1c7c317c97a8352c
S Watson <scottwatson1985@live.com>
2023-02-14 09:18:48 +0000

    starting to add in drop down sections so the main content is not on show until icon hovered/clicked on.  needs more work

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,6 @@
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
-function myFunction() {
+function menuDropdown() {
     document.getElementById("myDropdown").classList.toggle("show");
   }
   
@@ -17,3 +17,4 @@ if (!event.target.matches('#navTwo')) {
     }
 }
 }
+
